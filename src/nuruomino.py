2ce63bef50df8967aea265a25765fac3a86fe3ab
ist1110135 <manuel.semedo@tecnico.ulisboa.pt>
2025-05-08 15:35:46 +0100

    board_methods!!!

--- src/nuruomino.py
+++ src/nuruomino.py
@@ -8,12 +8,14 @@
 
 from search import *
 from sys import stdin
+from collections import defaultdict
 
 class NuruominoState:
     state_id = 0
 
-    def __init__(self, board):
+    def __init__(self, board, regions):
         self.board = board
+        self.regions = regions
         self.id = Nuruomino.state_id
         Nuruomino.state_id += 1
 
@@ -25,19 +27,56 @@ class NuruominoState:
 class Board:
     """Representação interna de um tabuleiro do Puzzle Nuruomino."""
 
+    def get_value(self, row:int, col:int):
+        return self[row][col]
     
-    def adjacent_regions(self, region:int) -> list:
+    def adjacent_regions(self, regions, region:int) -> list:
         """Devolve uma lista das regiões que fazem fronteira com a região enviada no argumento."""
         #TODO
+        positions = regions[region]
+        print(f"pos: {positions}")
+        adj_regions = []
+
+        for i, j in positions:
+            adj_val = Board.adjacent_values(self, i, j)
+            print(f"adj val on pos {i, j}: {adj_val}")
+            
+            for r in adj_val:
+                if r not in adj_regions and r != region:
+                    adj_regions.append(r)
+        
+        return adj_regions
+
         pass
     
     def adjacent_positions(self, row:int, col:int) -> list:
         """Devolve as posições adjacentes à região, em todas as direções, incluindo diagonais."""
         #TODO
+        deltas = [(-1, -1), (-1, 0), (-1, 1),
+              ( 0, -1),          ( 0, 1),
+              ( 1, -1), ( 1, 0), ( 1, 1)]
+
+        size = len(board)
+
+        adj_pos = []
+        for dr, dc in deltas:
+            r, c = row + dr, col + dc
+            if 0 <= r < size and 0 <= c < size:
+                adj_pos.append((r, c))
+
+        return adj_pos
+    
         pass
 
     def adjacent_values(self, row:int, col:int) -> list:
         """Devolve os valores das celulas adjacentes à região, em todas as direções, incluindo diagonais."""
+        adj_val = []
+        adj_pos = Board.adjacent_positions(self, row, col)
+        for i,j in adj_pos:
+            adj_val.append(Board.get_value(self, i, j))
+        
+        return adj_val
+    
         #TODO
         pass
     
@@ -54,26 +93,30 @@ class Board:
             > line = stdin.readline().split()
         """
         board = []
-        regions = []
+    
         for line in stdin:
             if not line.strip():
                 continue
             board.append([int(x) for x in line.strip().split()])
-            regions.append([(line,) for x in line.strip().split()])
-        return board
+
+        # Agora que temos a board, extraímos as regiões
+        region_dict = defaultdict(list)
+        for i, row in enumerate(board):
+            for j, val in enumerate(row):
+                region_dict[val].append((i, j))
+        
+        # Lista de regiões (cada uma é uma lista de coordenadas)
+        #regions = list(region_dict.values())
+
+        return board, region_dict
     
         #TODO
         pass
 
-    """def parse_instance2():
+    def print_instance(self):
+        for row in self:
+            print(" ".join(str(x) for x in row))
 
-        region_matrix = []
-        for line in stdin:
-            if not line.strip():
-                continue
-            region_matrix.append([int(x) for x in line.strip().split()])
-        return Board(region_matrix)    
-    """
     # TODO: outros metodos da classe Board
 
 class Nuruomino(Problem):
@@ -115,7 +158,17 @@ class Nuruomino(Problem):
         pass
 
 if __name__ == "__main__":
-    board = Board.parse_instance()
-    print(board)    
-    #for row in board.region_matrix:
-    #    print(row)
+    board, regions = Board.parse_instance()
+    Board.print_instance(board)
+    # Exemplo de impressão das regiões
+    for i, region in enumerate(regions):
+        print(f"Região {i+1}: {region}")
+
+    a = 1
+    b = 2
+    print("\nAdj_pos:")
+    print(Board.adjacent_positions(board, a, b))
+    print("\nAdj_values:")
+    print(Board.adjacent_values(board, a, b))
+    print("\nAdj_regions:")
+    print(Board.adjacent_regions(board, regions, 5))
