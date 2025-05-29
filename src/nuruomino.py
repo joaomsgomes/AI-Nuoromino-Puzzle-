411d757c2b7c96078dd229ff22e7b9bc31ca65c2
ist1110135 <manuel.semedo@tecnico.ulisboa.pt>
2025-05-29 21:49:32 +0100

    READdown

--- src/nuruomino.py
+++ src/nuruomino.py
@@ -13,6 +13,7 @@ import copy
 from collections import deque
 import time
 
+
 TETROMINO_SHAPES = {
     'I': [
         [(0,0), (0,1), (0,2), (0,3)],
@@ -59,7 +60,9 @@ class NuruominoState:
         self.id = NuruominoState.state_id
         NuruominoState.state_id += 1
         self.bad_path = False
-        self.region_priority = 0
+        self.only_letter = 0
+        self.action_region_size = 0
+        self.region_actions = 0
 
     def __lt__(self, other):
         """ Este método é utilizado em caso de empate na gestão da lista
@@ -227,9 +230,7 @@ class Board:
             return True
         return False
     
-        
     def fill_tetromino_regions(self):
-
         """Preenche as regioões do tabuleiro com tamanho 4 e remove as suas posições da região e adiciona a peça ao placed_pieces."""
         for r in list(self.regions.keys()):
             if len(self.regions[r]) == 4:
@@ -238,7 +239,7 @@ class Board:
                 self.placed_pieces.append((piece_letter, self.regions[r]))
                 self.regions.pop(r)
                 
-    
+                
     def place_piece(grid, piece, positions):
         for i, j in positions:
             grid[i][j] = piece
@@ -285,28 +286,33 @@ class Board:
         
         """Devolve as peças que podem ser colocadas na região atendendo às regras do L.I.T.S ."""
         
-        grid = [row[:] for row in self.grid]
+        board = self.clone()
 
         filtered_pieces = []
 
         for piece in possible_pieces:
 
             piece_type, positions = piece
+            aux_board = board.clone()
 
-            # Checa adjacências com o aux_grid diretamente
+            for i, j in positions:
+                aux_board.grid[i][j] = 'P'
+
+            # Checa adjacências com o aux_board.grid diretamente
             has_invalid_adj = any(
-                piece_type in self.adjacent_values(row, col)
+                piece_type in aux_board.adjacent_values(row, col)    #RECEBE BOARD????
                 for row, col in positions
             )
 
+
             if has_invalid_adj:
+                #Board.print_instance(aux_board.grid)
+                #print("Invalid adjacency for piece:", piece_type, "at positions:", positions)
                 continue
 
-            aux_grid = [row[:] for row in grid]
-
-            Board.place_piece(aux_grid, piece_type, positions)
-
-            forms_square = any(Board.is_square(aux_grid, row, col) for row, col in positions)
+            Board.place_piece(aux_board.grid, piece_type, positions)
+            
+            forms_square = any(Board.is_square(aux_board.grid, row, col) for row, col in positions)
             if forms_square:
                 continue
 
@@ -327,42 +333,6 @@ class Board:
                 return True
         
         return False
-            
-    def fixed_positions(self, affected_regions):
-        #COLOCAR P's na grid
-        
-        for region in affected_regions:
-                        
-            filtered_actions = self.filter_actions(self.possible_pieces[region])
-            
-            if len(filtered_actions) == 0:
-                return False
-            
-            self.possible_pieces[region] = filtered_actions
-
-            visited_positions = set()
-
-            region_pieces = self.possible_pieces[region]
-            fixed_positions = region_pieces[0][1]
-
-            for _, positions in region_pieces:
-                aux = []
-                for (i, j) in positions:
-                    if (i, j) not in visited_positions:
-                        visited_positions.add((i,j))
-                    if (i, j) in fixed_positions:
-                        aux.append((i, j))
-                fixed_positions = aux
-            
-            for i, j in fixed_positions:
-                if self.grid[i][j] != 'P':
-                    self.grid[i][j] = 'P'
-
-            #self.regions[region] = list(visited_positions)
-    
-            
-        return True
-
 
     def clone(self):   
 
@@ -381,6 +351,76 @@ class Board:
 
     # TODO: outros metodos da classe Board
 
+
+def fixed_positions(state: NuruominoState, adj_regions):
+        #COLOCAR P's na grid
+    queue = deque(adj_regions)
+    queued_regions = set(adj_regions)
+
+    #print("State:", state.id)
+    
+    while queue:
+        #print("Queue:", queue)
+        region = queue.popleft()
+        queued_regions.remove(region)
+
+        #print("Region:", region)
+                    
+        filtered_actions = state.board.filter_actions(state.board.possible_pieces[region])
+        
+        if len(filtered_actions) == 0:
+            #print("No possible pieces for region", region)
+            return False
+        
+        state.board.possible_pieces[region] = filtered_actions
+
+        region_pieces = state.board.possible_pieces[region]
+        fixed_positions = region_pieces[0][1]
+        fixed_letter = region_pieces[0][0]
+
+        for letter, positions in region_pieces:
+            aux = []
+            for (i, j) in positions:
+                if (i, j) in fixed_positions:
+                    aux.append((i, j))
+                if fixed_letter != letter:
+                    fixed_letter = 'P'
+            fixed_positions = aux
+
+        # Se não houver posições fixas, continuar para a próxima região
+        if len(fixed_positions) == 0:
+            #print("No fixed positions for region", region)
+            #print("TEsted on board:")
+            #Board.print_instance(state.board.grid)
+            continue
+        
+        # Se houver 4 posições fixas, colocar a peça e continuar para a próxima regiã0
+        if len(fixed_positions) == 4:
+            Board.place_piece(state.board.grid, fixed_letter, fixed_positions)
+            state.board.placed_pieces.append((fixed_letter, fixed_positions))
+            state.board.regions.pop(region)
+            state.board.possible_pieces.pop(region)
+
+            for r in state.board.region_adj_regions[region]:
+                if r not in queued_regions and r in state.board.regions.keys():
+                    queue.append(r)
+                    queued_regions.add(r)
+        
+        else:
+            # CC, colocar P's
+            for i, j in fixed_positions:
+                if state.board.grid[i][j] != fixed_letter:
+                    state.board.grid[i][j] = fixed_letter    #TODO: REVER Ps / LETTTERS na grid
+                    state.only_letter += len(fixed_positions)
+
+    
+
+        #self.regions[region] = list(visited_positions)
+
+    #Board.print_regions(self.possible_pieces)
+        
+    return True
+
 class Nuruomino(Problem):
 
     def __init__(self, board: Board):
@@ -389,15 +429,14 @@ class Nuruomino(Problem):
         self.initial = NuruominoState(board)
         self.initial.state_id = 0 # ID do estado inicial
 
-
         self.board.fill_tetromino_regions()
         board.set_possible_pieces()
         board.set_adjacent_regions()
         
-        self.board.fixed_positions(list(self.board.regions.keys())) #Estado inicial de qualquer no
+        fixed_positions(self.initial, list(self.board.regions.keys())) #Estado inicial de qualquer no
         
-        
-
+        Board.print_instance(self.board.grid)
+        #time.sleep(1)
         
         #TODO
         pass 
@@ -405,7 +444,6 @@ class Nuruomino(Problem):
     def actions(self, state: NuruominoState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
-
         all_actions = []
         for region in state.board.regions:
 
@@ -415,7 +453,9 @@ class Nuruomino(Problem):
                     
                     all_actions.append((region, piece, pos))
 
-        #time.sleep(1)
+        time.sleep(2)
+        print("State ID:", state.id)
+        Board.print_instance(state.board.grid)
         
         return all_actions
     
@@ -427,6 +467,8 @@ class Nuruomino(Problem):
         self.actions(state)."""
         region, piece, positions = action
 
+        #Board.print_instance(state.board.grid)
+
         if len(state.board.regions[region]) < 4:
             return None
         
@@ -436,18 +478,20 @@ class Nuruomino(Problem):
         Board.place_piece(new_state.board.grid, piece, positions)
         new_state.board.placed_pieces.append((piece, positions))
 
-        new_state.region_priority = Nuruomino.find_region_priority(state.board.regions, region)
-    
+        new_state.action_region_size = len(state.board.regions[region])
+        new_state.region_actions = len(state.board.possible_pieces[region])
         new_state.board.regions.pop(region)
         new_state.board.possible_pieces.pop(region)
 
         
         affected_regions = [r for r in new_state.board.region_adj_regions[region] if r in new_state.board.regions.keys()]
 
-        if not new_state.board.fixed_positions(affected_regions):
+        if not fixed_positions(new_state, affected_regions):
             new_state.bad_path = True
 
         #print("\n\n\n")
+        #print("New State ID:", new_state.id)
+        #Board.print_instance(new_state.board.grid)
 
         return new_state
               
@@ -455,6 +499,7 @@ class Nuruomino(Problem):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas de acordo com as regras do problema."""
+
         if len(state.board.regions) > 0:
             return False
         
@@ -503,68 +548,56 @@ class Nuruomino(Problem):
         if len(connections) == 0:
             return 0.5
         return len(connections)
-
-    def find_region_priority(regions, action_region):
-    
-        sorted_regions = sorted(regions.keys(),
-                            key=lambda r: len(regions[r]))
-
-        #Board.print_instance(node.state.board.grid)
-        #print(sorted_regions)
-        #print("all regions sorted", sorted_regions)
-        h = 0
-        for reg in sorted_regions:
-            #print("reg", reg)
-            if action_region == reg:
-                return h
-            
-            h+=1
-        
-        return h
     
     def h_bad_path(state: NuruominoState):
         if state.bad_path:
             return 1000
         return 1
     
-    def h_region_priority(state: NuruominoState):
-        if state.region_priority == 0:
-            return 0.5
-        return state.region_priority
-    
-    def h_state_id(state: NuruominoState):
-        if state.state_id == 0:
-            return 0.5
-        return state.state_id
-    
     def h(self, node: Node):
         #print("Node_ID and Action:", node.state.id, node.action)
 
         """Função heuristica utilizada para a procura A*."""
-      
-        h1 = 1 / Nuruomino.h_state_connections(node.state)
 
-        h2 = Nuruomino.h_region_priority(node.state)
+        if node.state.id == 0:
+            return 1000
 
-        #h3 = 1 / Nuruomino.h_state_id(node.state)
+        # Número de conexões entre peças (quanto mais melhor)
+        connections = Nuruomino.h_state_connections(node.state)
+        # Quantidade de novas posições fixas descobertas (quanto mais melhor)
+        new_Ps = node.state.only_letter
+        region_size = node.state.action_region_size
+        # Número de regiões por preencher (quanto menos melhor)
+        regions_left = len(node.state.board.regions)
+        # Quantidade de ações possíveis na região (quanto menos melhor)
+        num_actions = node.state.region_actions
+        # Tamanho médio das regiões adjacentes (quanto menos melhor)
+        #adj_regions = Nuruomino.h_adj_regions_priority(node.state)
 
-        h3 = len(node.state.board.regions) # Priorizar estados com menos regiões por preencher
-        #h4 = Nuruomino.broken_regions(node) # Por ver se vale a pena
-        h4 = Nuruomino.h_bad_path(node.state)
+        # Penalização, caminho sem solução
+        critical = Nuruomino.h_bad_path(node.state)
+        
+        
+        #depth_bonus = 1 / (node.depth*node.depth*node.depth + 1)  # Evitar que a profundidade penalize muito a heurística
 
-        #print("Node_id:", node.state.id)
-        #print("Node_depth:", node.depth)
-        #print("h1:", h1, "h2:", h2, "h3:", h3, "h4:", h4)
-        #print("Node State Board:")
-        #print(f"h1: {h1}, h2: {h2}, h3: {h3}, h4: {h4}")
-        #Board.print_regions(node.state.board)
-        w1 = 0.1
-        w2 = 2.0
-        w3 = 1.0
+        h = (
+            1.0 * num_actions +
+            2.0 * region_size +
+            3.0 * regions_left -
+            1.0 * new_Ps +
 
-        h = ( w1*h1 + w2*h2 + w3*h3) * h4
+            1.0 * critical
+        )
 
-        #rint("Heuristic value:", h)
+        h -= 0.0001 * node.depth
+
+        if h < 100:
+            print("Node_id:", node.state.id)
+            print("Action:", node.action)
+            print(f" H region size: {region_size} \n| Actions: {num_actions} \n| Regions left: {regions_left} \n| New Ps: {new_Ps} \n| Connections: {connections} \n| Critical: {critical}")
+            print("F(n):", h)
+            Board.print_instance(node.state.board.grid)
+            print("\n")
         #print("\n")
 
         return h
@@ -580,5 +613,19 @@ if __name__ == "__main__":
     goal_node = astar_search(problem)
     #print("SOLUTION: ")
     Board.print_instance(problem.board.grid)
+
+
+    # PROBLEM:   Ações diferentes geram os mesmos estados equivalentes, mas formados por ordens diferentes
+    # CAUSE:        Função Fixed Positions resolve as implicações de regiões imediatamente, em vez de só colocar novas peças em estados futuros
+    # SOLUTION:     Mudar abordagem? Misturar com versão anterior?
+    # Esta versão:      Mais condições no fixed_positions (peças únicas na região mas em posições diferentes)
+    # Anterior versão:    Colocar peças apenas em novos estados!
+    '''
+    Exatamente, é isso mesmo!
+    O teu algoritmo está a considerar estados equivalentes como diferentes porque,
+    apesar do grid ser igual, outros atributos do estado (como placed_pieces, regions,
+    possible_pieces, etc.) podem estar em ordens diferentes ou conter informação redundante,
+    levando a diferentes hashes ou comparações
+    '''
     
 
